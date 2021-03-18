6acb9e6f2edda8d2ae7c98620e67f4f108752821
RodMiley <rodmiley5@hotmail.com>
2021-03-18 11:21:57 +0000

    content update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Compensation recovery unit',
+  serviceName: 'Compensation Recovery Unit',
 
   // Default port that prototype runs on
   port: '3000',
