1d290e7b5d50271aabefa37a1c111ba94dcb0c6a
RodMiley <rodmiley5@hotmail.com>
2021-04-01 12:01:01 +0100

    content update

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
+
+
 module.exports = router
