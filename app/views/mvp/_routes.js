ae6982a69815cd56dcf15ddbb0c242862e73849c
RodMiley <rodmiley5@hotmail.com>
2021-06-17 14:25:17 +0100

    Case details tabs

--- app/views/mvp/_routes.js
+++ app/views/mvp/_routes.js
@@ -20,3 +20,60 @@ router.get('/*', function (req, res, next) {
     res.locals.cases = crucases
      next()
       })
+
+
+
+
+      router.get('/case/task-confirm', function (req, res, next) {
+
+        let findCase = {}
+
+        for (let i = 0; i < crucases.length; i++ ) {
+        if(crucases[i].reference === req.query.reference){
+          findCase = crucases[i];
+        }
+    
+        }
+    
+  
+        let status = req.session.data['taskcompleted']
+        if(status === 'Close case on new cru'){
+          findCase.task ='completed-closed' 
+          }   
+
+          if(status === 'Issue certificate'){
+            findCase.task ='completed-open' 
+            }  
+       
+        res.locals.case = findCase
+        next()
+         })  
+         
+         
+         router.get('/cru-ops-service/reset-tasks-confirm', function (req, res, next) {
+
+          let findCase = {}
+  
+          for (let i = 0; i < crucases.length; i++ ) {
+          if(crucases[i].task === 'completed-closed'){
+            findCase = crucases[i];
+          }
+
+          if(crucases[i].task === 'completed-open'){
+            findCase = crucases[i];
+          }
+      
+          }
+      
+    
+          let status = req.session.data['reset-task']
+          if(status === 'yes'){
+            findCase.task ='yes'
+            }   
+         
+          res.locals.case = findCase
+          next()
+           })  
+         
+         
+  
