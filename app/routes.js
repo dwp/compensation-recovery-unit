8b06223be220da8679d29fdb4941696315839f16
RodMiley <rodmiley5@hotmail.com>
2021-09-22 07:48:58 +0100

    Sandboxed v3

--- app/routes.js
+++ app/routes.js
@@ -27,6 +27,7 @@ router.use('/', (req, res, next) => {
 
 
   router.use('/current', require('./views/current/_routes'));
+  router.use('/v4', require('./views/v4/_routes'));
   router.use('/v3', require('./views/v3/_routes'));
   router.use('/v2-compensators', require('./views/v2-compensators/_routes'));
   router.use('/v1-compensators', require('./views/v1-compensators/_routes'));
